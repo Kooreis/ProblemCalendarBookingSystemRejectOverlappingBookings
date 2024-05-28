04bbd0fce40ab92de6abaf5802ac6fad2be177be
Kooreis <kooreis.official@gmail.com>
2024-05-28 09:48:09 -0500

    Created a Calendar class with a private list of Booking objects. Added a method to add a new booking to the list, with checks for overlapping bookings and invalid start/end times.

--- solution.cs
+++ solution.cs
@@ -1,9 +1,26 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-
-public class Booking
+public class Calendar
 {
-    public DateTime Start { get; set; }
-    public DateTime End { get; set; }
+    private List<Booking> bookings = new List<Booking>();
+
+    public bool AddBooking(DateTime start, DateTime end)
+    {
+        if (start >= end)
+        {
+            Console.WriteLine("Invalid booking. Start time must be earlier than end time.");
+            return false;
+        }
+
+        foreach (var booking in bookings)
+        {
+            if ((start >= booking.Start && start < booking.End) || (end > booking.Start && end <= booking.End))
+            {
+                Console.WriteLine("Booking overlaps with an existing booking.");
+                return false;
+            }
+        }
+
+        bookings.Add(new Booking { Start = start, End = end });
+        Console.WriteLine("Booking added successfully.");
+        return true;
+    }
 }
